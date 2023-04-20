b01d05f0cbbdf462bce7ba5c9141e61725b12bc2
Mehdi Kaalat <mehdikaalat@gmail.com>
2023-04-20 22:48:43 +0000

    add some products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
-
+export class ProductsComponent implements OnInit {
+  products! : Array<any>;
+  constructor() {}
+  
+  ngOnInit():void {
+    this.products = [
+      {id : 1, name: "Computer", price : 6500},
+      {id : 2, name: "Printer", price : 1200},
+      {id : 3, name: "Smart Phone", price : 1400}
+    ];
+  }
 }
